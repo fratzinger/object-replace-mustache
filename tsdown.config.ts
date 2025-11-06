6abe46caee7d63e604a9ccfccbec430936f7241b
fratzinger <22286818+fratzinger@users.noreply.github.com>
2025-11-06 08:47:54 +0100

    chore: esm-only, tsdown, update deps

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+import pkg from "./package.json";
+
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  outDir: "./dist",
+  format: ["esm"],
+  dts: true,
+  external: [
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.devDependencies),
+  ],
+});
